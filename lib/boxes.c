34428d846adb49973f761bf866fb0fe16c9e2296
Lord Baraa <pub@lordbaraa.net>
2026-07-23 23:51:26 +0300

    Use arrays for adding utility classes

--- lib/boxes.c
+++ lib/boxes.c
@@ -1,21 +1,38 @@
 #include "boxes.h"
+#include "arrays.h"
+#include "css.h"
 #include "utility_classes.h"
 
 void init_boxes_classes() {
-  add_util_class("float-start", ".%s {float: inline-start;}",
-                 SINGLE_REPLACEMENT);
-  add_util_class("!float-start", ".%s {float: inline-start !important;}",
-                 SINGLE_REPLACEMENT);
-  add_util_class("float-end", ".%s {float: inline-end;}", SINGLE_REPLACEMENT);
-  add_util_class("!float-end", ".%s {float: inline-end !important;}",
-                 SINGLE_REPLACEMENT);
-  add_util_class("float-right", ".%s {float: right;}", SINGLE_REPLACEMENT);
-  add_util_class("!float-right", ".%s {float: right !important;}",
-                 SINGLE_REPLACEMENT);
-  add_util_class("float-left", ".%s {float: left;}", SINGLE_REPLACEMENT);
-  add_util_class("!float-left", ".%s {float: left !important;}",
-                 SINGLE_REPLACEMENT);
-  add_util_class("float-none", ".%s {float: none;}", SINGLE_REPLACEMENT);
-  add_util_class("!float-none", ".%s {float: none !important;}",
-                 SINGLE_REPLACEMENT);
+  array *matcher_css_pairs = array_new(0, PAIRS);
+  array *tmp = NULL;
+
+  tmp = get_class_and_fmt_variations("float-start", "float", "inline-start",
+                                     NORMAL | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+  array_shallow_destroy(tmp);
+  tmp = get_class_and_fmt_variations("float-end", "float", "inline-end ",
+                                     NORMAL | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+  array_shallow_destroy(tmp);
+  tmp = get_class_and_fmt_variations("float-right", "float", "right",
+                                     NORMAL | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+  array_shallow_destroy(tmp);
+  tmp = get_class_and_fmt_variations("float-left", "float", "left",
+                                     NORMAL | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+  array_shallow_destroy(tmp);
+  tmp = get_class_and_fmt_variations("float-none", "float", "none",
+                                     NORMAL | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+
+  ARRAY_ITER(matcher_css_pairs, it) {
+    add_util_class(it.pair->strstr->first, it.pair->strstr->second,
+                   SINGLE_REPLACEMENT);
+    pair_destroy((pair *)it.pair);
+  }
+
+  array_shallow_destroy(tmp);
+  array_shallow_destroy(matcher_css_pairs);
 }
--- lib/sizing.c
+++ lib/sizing.c
@@ -1,31 +1,50 @@
 #include "sizing.h"
+#include "arrays.h"
+#include "css.h"
 #include "utility_classes.h"
+#include <stdio.h>
 
 void init_sizing_classes() {
   // free ballling
-  add_util_class("w", ".%s {width: %s;}", SINGLE_REPLACEMENT);
-  add_util_class("!w", ".%s {width: %s !important;}", SINGLE_REPLACEMENT);
-  add_util_class("-w", ".%s {width: -%s;}", SINGLE_REPLACEMENT);
-  add_util_class("h", ".%s {height: %s;}", SINGLE_REPLACEMENT);
-  add_util_class("!h", ".%s {height: %s !important;}", SINGLE_REPLACEMENT);
-  add_util_class("-h", ".%s {height: -%s;}", SINGLE_REPLACEMENT);
-  add_util_class("max-w", ".%s {max-width: %s;}", SINGLE_REPLACEMENT);
-  add_util_class("!max-w", ".%s {max-width: %s !important;}",
-                 SINGLE_REPLACEMENT);
-  add_util_class("-max-w", ".%s {max-width: -%s;}", SINGLE_REPLACEMENT);
-  add_util_class("max-h", ".%s {max-height: %s;}", SINGLE_REPLACEMENT);
-  add_util_class("!max-h", ".%s {max-height: %s !important;}",
-                 SINGLE_REPLACEMENT);
-  add_util_class("-max-h", ".%s {max-height: -%s;}", SINGLE_REPLACEMENT);
-  add_util_class("min-w", ".%s {min-width: %s;}", SINGLE_REPLACEMENT);
-  add_util_class("!min-w", ".%s {min-width: %s !important;}",
-                 SINGLE_REPLACEMENT);
-  add_util_class("-min-w", ".%s {min-width: -%s;}", SINGLE_REPLACEMENT);
-  add_util_class("min-h", ".%s {min-height: %s;}", SINGLE_REPLACEMENT);
-  add_util_class("!min-h", ".%s {min-height: %s !important;}",
-                 SINGLE_REPLACEMENT);
-  add_util_class("-min-h", ".%s {min-height: -%s;}", SINGLE_REPLACEMENT);
+  array *matcher_css_pairs = array_new(0, PAIRS);
+  array *tmp = NULL;
+  int stat = 0;
+
+  tmp = get_class_and_fmt_variations("w", "width", "%s",
+                                     NORMAL | NEGATIVE | IMPORTANT);
+
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+  array_shallow_destroy(tmp);
+  tmp = get_class_and_fmt_variations("h", "height", "%s",
+                                     NORMAL | NEGATIVE | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+  array_shallow_destroy(tmp);
+  tmp = get_class_and_fmt_variations("max-w", "max-width", "%s",
+                                     NORMAL | NEGATIVE | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+  array_shallow_destroy(tmp);
+  tmp = get_class_and_fmt_variations("min-w", "min-width", "%s",
+                                     NORMAL | NEGATIVE | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+  array_shallow_destroy(tmp);
+  tmp = get_class_and_fmt_variations("max-h", "max-height", "%s",
+                                     NORMAL | NEGATIVE | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
+  array_shallow_destroy(tmp);
+  tmp = get_class_and_fmt_variations("min-h", "min-height", "%s",
+                                     NORMAL | NEGATIVE | IMPORTANT);
+  ARRAY_ITER(tmp, it) { array_push_pair(matcher_css_pairs, it.pair); }
 
   // predefined
   add_util_class("w-24", ".w-24 {width: 92px;}", NO_REPLACEMENT);
+
+  // hmm
+  ARRAY_ITER(matcher_css_pairs, it) {
+    add_util_class(it.pair->strstr->first, it.pair->strstr->second,
+                   SINGLE_REPLACEMENT);
+    pair_destroy((pair *)it.pair);
+  }
+
+  array_shallow_destroy(tmp);
+  array_shallow_destroy(matcher_css_pairs);
 }
