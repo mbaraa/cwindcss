380b14874c6218360a2795a9b367f221a6143240
Lord Baraa <pub@lordbaraa.net>
2026-07-24 01:11:45 +0300

    Rename cli main file

---
+++ cli/main.c
@@ -0,0 +1,25 @@
+#include "arrays.h"
+#include "css.h"
+#include "cwindcss.h"
+#include "sizing.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    perror("usage:\n\tcwindcss <dir>");
+    return 1;
+  }
+
+  cwind_init();
+
+  int stat = cwind_generate_output_css_file(argv[1], ".html", "./cwind.css");
+  if (stat != CWIND_OK) {
+    perror("oopsie something went wrong");
+    return 1;
+  }
+
+  cwind_destroy();
+
+  return 0;
+}
